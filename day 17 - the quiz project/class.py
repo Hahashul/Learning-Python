8728922b7100294e726226c4ef0d29644fbd2bb5
Hahashul <harshuladwani@gmail.com>
2026-06-05 18:24:18 +0530

    working with classes

--- day 17 - the quiz project/class.py
+++ day 17 - the quiz project/class.py
@@ -1,4 +1,19 @@
 class User:
-    pass
+    def __init__(self,name):
+        print("object is created")
+        self.name=name
+        self.follower=0
+        self.following=0
 
-user1 = User()
+    def follow(self,user):
+        user.follower+=1
+        self.following+=1
+
+
+user1 = User("harshul")
+user2=User("ayushi")
+user2.follow(user1)
+print(user1.follower)
+print(user2.follower)
+print(user1.following)
+print(user2.following)
