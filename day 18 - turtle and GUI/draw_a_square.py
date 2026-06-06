1a3e2ddc892476f874fd18aa8b9df0586f386b5c
Hahashul <harshuladwani@gmail.com>
2026-06-06 16:13:47 +0530

    initial commit

---
+++ day 18 - turtle and GUI/draw_a_square.py
@@ -0,0 +1,17 @@
+from turtle import Turtle,Screen
+
+hahshul=Turtle()
+screen=Screen()
+
+hahshul.forward(100)
+hahshul.left(90)
+hahshul.forward(100)
+hahshul.left(90)
+hahshul.forward(100)
+hahshul.left(90)
+hahshul.forward(100)
+hahshul.left(90)
+
+
+
+screen.exitonclick()
